4ea9824668442be40adaa931a15d333b9e7ebf18
founder-space <naveenkumar07112002@gmail.com>
2024-12-31 22:36:20 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Logged in!"); // For now, just alerting that login is successful
+});
+    
